a4f0616ace06bf840c05e27e2e04249af124232a
Stanislav Pirx <stanislav.pirx@gmail.com>
2021-07-16 14:07:54 +0300

    update schemas
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- contracts/basset_vault_config_holder/examples/schema.rs
+++ contracts/basset_vault_config_holder/examples/schema.rs
@@ -1,10 +1,10 @@
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use basset_vault::nasset_token_config_holder::{
-    AnyoneMsg, ConfigResponse, ExecuteMsg, GovernanceMsg, InstantiateMsg, QueryMsg,
+use basset_vault::basset_vault_config_holder::{
+    ConfigResponse, ExecuteMsg, GovernanceMsg, InstantiateMsg, QueryMsg,
 };
+use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -14,7 +14,6 @@ fn main() {
 
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
-    export_schema(&schema_for!(AnyoneMsg), &out_dir);
     export_schema(&schema_for!(GovernanceMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(ConfigResponse), &out_dir);
