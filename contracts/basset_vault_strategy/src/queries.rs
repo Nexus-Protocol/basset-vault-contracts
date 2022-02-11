05b2795e677782c9200de369de2734a94cf5fca9
Victor <victor@lingle.io>
2022-02-11 19:47:47 +0300

    Fix warning

--- contracts/basset_vault_strategy/src/queries.rs
+++ contracts/basset_vault_strategy/src/queries.rs
@@ -2,7 +2,7 @@ use crate::price::{query_price, PriceResponse};
 use basset_vault::anchor::borrow_apr::BorrowNetApr;
 use basset_vault::basset_vault_strategy::{BorrowerActionResponse, ConfigResponse};
 use cosmwasm_bignumber::{Decimal256, Uint256};
-use cosmwasm_std::{Deps, Env, StdError, StdResult, Timestamp};
+use cosmwasm_std::{Deps, Env, StdResult, Timestamp};
 
 use crate::state::{load_config, Config};
 
